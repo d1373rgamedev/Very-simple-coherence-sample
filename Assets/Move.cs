bb43f15f45a30d95e0e6f54123f2fb7bc92a8b38
d1373r <d1373rgamedev@gedankenkraft.com>
2023-01-31 00:03:45 +0100

    Improvement of PlayerCube Controller

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float speed = 1f;
 
+    private float horizontalMovement;
+    private float verticalMovement;
+
+    // A field editable from inside Unity with a default value of 5
+    public float speed = 5.0f;
+
+    // How much will the player slide on the ground
+    // The lower the value, the greater distance the user will slide
+    public float drag;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        // This will detect forward and backward movement
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-        var spf = speed * Time.deltaTime;
+        // This will detect sideways movement
+        verticalMovement = Input.GetAxisRaw("Vertical");
 
-        transform.position += transform.forward * (v * spf);
-        transform.position += transform.right * (h * spf);
+        // Calculate the direction to move the player
+        Vector3 movementDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+
+        // Move the player
+        rb.AddForce(movementDirection * speed, ForceMode.Force);
+        // Apply drag
+        rb.drag = drag;
     }
 }
